16bc056262debc70914b53e6c4c7e6bb6b667aca
Roger Tosbotn <rogertosb@gmail.com>
2020-06-16 14:13:58 +0200

    added mock data in the form of a line and a bar

--- dash-tutorial.py
+++ dash-tutorial.py
@@ -4,7 +4,20 @@ import dash_html_components as html
 
 app = dash.Dash()
 
-app.layout = html.Div('Dash tutorials')
+app.layout = html.Div(children=[
+  html.H1('Dash tutorials'),
+  dcc.Graph(id='example',
+    figure = {
+    'data': [
+    {'x':[1, 2, 3, 4, 5], 'y':[ 5, 10, 15, 20, 25], 'type':'line', 'name': 'boats'},
+    {'x':[1, 2, 3, 4, 5], 'y':[ 1, 2, 3, 4, 5], 'type':'bar', 'name': 'cars'},
+    ],
+    'layout': {
+      'title':'Basic dash example'
+    }
+
+    })
+  ])
 
 if __name__ == '__main__':
   app.run_server(debug=True)
