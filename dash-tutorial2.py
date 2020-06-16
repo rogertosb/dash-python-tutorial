67a4c37123fc1a163dfc9c036952b1fce89ef499
Roger Tosbotn <rogertosb@gmail.com>
2020-06-16 14:17:29 +0200

    dash tut 2 includes input/outputs

---
+++ dash-tutorial2.py
@@ -0,0 +1,13 @@
+import dash
+from dash.dependencies import Input, Output
+import dash_core_components as dcc
+import dash_html_components as html
+
+app = dash.Dash()
+
+app.layout = html.Div(children=[
+  dcc.Input(id='input', value='Enter value', type='text')
+ ])
+
+if __name__ == '__main__':
+  app.run_server(debug=True)
